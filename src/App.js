c103a610272ed656fff48d587c1b9d553b66feab
Sarah Almahmid <hpsarontm@gmail.com>
2021-06-27 17:02:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./components/Home";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles";
 
+const theme = {
+  mainColor: "white",
+  backgroundColor: "#2A2D3E",
+  secondaryColor: "#DEDEDE",
+  lightColor: "#5E5E5E",
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <HomePage></HomePage>
+      </ThemeProvider>
     </div>
   );
 }
