87f5fc0d910381a97c1e69774cd798f24563a83e
Sarah Almahmid <hpsarontm@gmail.com>
2021-06-28 17:22:08 +0300

    more movies

--- src/movies.js
+++ src/movies.js
@@ -1,18 +1,36 @@
 const movies = [
   {
     id: 1,
-    name: "The Fellowship of The Ring",
+    name: "The Lord of The Rings: The Fellowship of The Ring",
     watched: true,
     rating: 5,
   },
   {
     id: 2,
-    name: "The Two Towers",
-    watched: false,
+    name: "The Lord of The Rings: The Two Towers",
+    watched: true,
+    rating: 5,
   },
   {
     id: 3,
-    name: "The Return of The King",
+    name: "The Lord of The Rings: The Return of The King",
+    watched: false,
+  },
+  {
+    id: 4,
+    name: "The Hobbit: An Unexpected Journey",
+    watched: true,
+    rating: 4,
+  },
+  {
+    id: 5,
+    name: "The Hobbit: The Desolation of Smaug",
+    watched: true,
+    rating: 3,
+  },
+  {
+    id: 5,
+    name: "The Hobbit: The Battle of the Five Armies",
     watched: false,
   },
 ];
